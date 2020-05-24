56b60ffcf4640e7ac0ffeca72b95edd623b3b13a
Martin Öbrink-Hansen <moh@trifork.com>
2020-05-24 16:08:48 +0200

    fixed generating heatmaps with year toggle possibility

--- heatmap.py
+++ heatmap.py
@@ -13,21 +13,17 @@ API_KEY = parser.get('Google', 'API_KEY')
 @click.option("--input", default="gpx", help="Specify an input folder. Defaults to `gpx`")
 @click.option("--filter", default=None, help="Specify a filter type. Defaults to no filter", type=click.Choice(['running', 'cycling', 'walking']))
 def main(output, input, filter):
-    points = load_points(input, filter)
-    generate_html(points, output)
+    workoutdata = load_workoutdata(input, filter)
+    generate_html(workoutdata, output)
 
-def load_points(folder, filter):
-    """Loads all gpx files into a list of points"""
+def load_workoutdata(folder, filter):
+    """Loads all gpx files into a dictionary of years and coordinates"""
 
     year_coord_dictionary = {}
     print (f"Loading files with type {filter}...") #Loads files with progressbar
     with click.progressbar(os.listdir(folder)) as bar:
         for filename in bar:
-            if (filename.endswith(".gpx")):
-                for key in year_coord_dictionary.keys():
-                    print ("key: ", key, "Size: ", sys.getsizeof(year_coord_dictionary[key]))
-                
-                #Verify file is a gpx file
+            if (filename.endswith(".gpx")):                
                 gpx_file = open(f'{folder}/' + filename)
                 gpx = gpxpy.parse(gpx_file)
                 for track in gpx.tracks:
@@ -37,26 +33,37 @@ def load_points(folder, filter):
                                 if point.time.year in year_coord_dictionary:
                                     year_coord_dictionary[point.time.year].append([float(point.latitude), float(point.longitude)])
                                 else:
-                                    year_coord_dictionary[point.time.year] = [float(point.latitude), float(point.longitude)]
+                                    year_coord_dictionary[point.time.year] = [[float(point.latitude), float(point.longitude)]]
 
     return (year_coord_dictionary)
 
-def get_outline():
-    """Reads in the html outline file"""
-    with open('map-outline.txt', 'r') as file:
-        outline = file.read()
-    return outline
-
-def generate_html(points, file_out):
-    """Generates a new html file with points"""
-    os.mkdir('output')
+def generate_html(workoutdata_dict, file_out):
+    """Generates a new html file with workout data"""
+    if not os.path.exists('output'):
+        os.makedirs('output')
     f = open(f"output/{file_out}.html", "w")
-    outline = get_outline()
-    google_points = ",\n".join([f"new google.maps.LatLng({point[0]}, {point[1]})" for point in points])
-    updated_content = outline.replace("LIST_OF_POINTS", google_points).replace("API_KEY", API_KEY)
+    template = get_template()
+    workoutdata_text = serialize_workoutdata(workoutdata_dict)
+    updated_content = template.replace("<WORKOUT_YEAR_COORDINATE_DICTIONARY>", workoutdata_text).replace("<API_KEY>", API_KEY)
     f.write(updated_content)
     f.close()
 
+def get_template():
+    """Reads in the html template file"""
+    with open('map-template.txt', 'r') as file:
+        template = file.read()
+    return template
+
+def serialize_workoutdata(workoutdata_dict):
+    serialized_data = "{\n"
+    for key in workoutdata_dict.keys():
+        serialized_data += " " + str(key) + " : [\n"
+        for value in workoutdata_dict[key]:
+            serialized_data += "  new google.maps.LatLng(" + str(value[0]) + "," + str(value[1]) + "),\n"
+        serialized_data += "],\n"
+    serialized_data += "};"
+
+    return serialized_data
 
 if __name__ == '__main__':
     main()
